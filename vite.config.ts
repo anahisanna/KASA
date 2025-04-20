40ef812f38ae2f996235070b07bd60941ca15967
anahisanna <anahisanna123@gmail.com>
2025-04-20 16:29:20 +0200

    fix: vite base path for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 import type { UserConfigExport } from 'vitest/config';
 
 const config: UserConfigExport = {
+  base: '', // 🔥 ESTA LÍNEA es la clave para Netlify
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/vitest.setup.ts'
+    setupFiles: './src/vitest.setup.ts',
   },
 };
 
